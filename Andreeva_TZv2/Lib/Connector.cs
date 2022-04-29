253387901b47878a36fae353ab1e54e3850ce917
nekto <nigma-ruslan2002@mail.ru>
2022-04-29 08:45:05 +0500

    f,j,f

---
+++ Andreeva_TZv2/Lib/Connector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andreeva_TZv2.Lib
+{
+    internal class Connector
+    {
+        private static BD.Administrator profile;
+        private static BD.DayAndNightEntities connector;
+
+        public Connector(BD.Administrator _profile)
+        {
+            _profile = profile;
+        }
+
+        public static BD.Administrator GetMyProfile()
+        {
+            return profile = new BD.Administrator();
+        }
+
+        public static BD.DayAndNightEntities GetModel()
+        {
+            return connector = new BD.DayAndNightEntities();
+        }
+
+
+
+    }
+}
